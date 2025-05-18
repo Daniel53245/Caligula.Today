a2336f925c6348a0eb4d869b471b31c46875a127
Daniel53245 <daniel53245@outlook.com>
2025-05-18 14:40:19 +1000

    Update scss files

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -6,21 +6,25 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import WorkIcon from '@mui/icons-material/Work';
 
 const Hero = () => {
+    const jumpTo = (e:React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        open(e.target.value)
+        }
     return (
         <div className="hero">
             <Navigation />
             <div className="content-container">
                 <h2>Boyu Dai</h2>
-                <p className="self-introdction">Some self introduction here </p>
+                <p className="self-introdction">Hi visitor, this is Dai. I am a software developer and game developer located in Canberra. Nice to meet you on my website.</p>
                 <div className="link-container">
                     <div className="link">
-                        <button><LinkedInIcon className="icon"/><p>Twitter</p></button>
+                        <button onClick={jumpTo} value="https://www.linkedin.com/in/boyudai"><LinkedInIcon className="icon" /><p>Linkedin</p></button>
                     </div>
                     <div>
-                        <button><XIcon className="icon"/><p>Twitter</p></button>
+                        <button onClick={jumpTo} disabled={true}><XIcon className="icon" /><p>Twitter</p></button>
                     </div>
                     <div>
-                        <button><WorkIcon className="icon" /><p>Twitter</p></button>
+                        <button onClick={jumpTo} value="https://github.com/Daniel53245"><WorkIcon className="icon" /><p>Github</p></button>
 
                     </div>
 
